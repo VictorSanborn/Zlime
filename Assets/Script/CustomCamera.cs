896f50e3588339d9d84d1719cc2f8c1318b424db
aggge12 <axel.lundberg.gbg@hotmail.com>
2020-12-02 22:24:18 +0100

    stuff

---
+++ Assets/Script/CustomCamera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCamera : MonoBehaviour
+{
+    GameObject focus;
+    public float allowedDistance;
+    public float speed;
+
+    public float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        focus = GameObject.FindGameObjectWithTag("Player");
+
+        transform.position = new Vector3(transform.position.x,height,transform.position.z);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        follow();
+
+        transform.LookAt(focus.transform);
+    }
+
+    public void follow(){
+
+        
+        Vector3 targetPosition = focus.transform.position;
+
+        float distance = Vector3.Distance (transform.position, targetPosition);
+
+        if (distance > allowedDistance){
+
+            Vector3 movement = Vector3.forward * speed * Time.deltaTime;
+            //movement.y = 0;
+            transform.Translate(movement);
+
+            transform.position = new Vector3(transform.position.x,height,transform.position.z);
+        }
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int turnSpeed;
+    public float speed;
+    public Vector3 rotation;
+    public float velocity;
+    private Vector3 Target;
+    private float target;
+    private float angle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update(){     
+        float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+        float moveVertical = Input.GetAxisRaw ("Vertical");
+
+        if(moveHorizontal != 0 || moveVertical != 0){
+            CalcRotation(moveHorizontal, moveVertical);
+                    
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+         
+        }
+
+    }
+        public void CalcRotation(float moveH, float moveV){
+
+            Vector3 camForward = Camera.main.transform.forward;
+            camForward.y = 0f;
+            Quaternion camRotationFlattened = Quaternion.LookRotation(camForward);
+
+            angle = Mathf.Atan2 (moveH, moveV) * Mathf.Rad2Deg; 
+
+            if (true){
+                Quaternion quaternionAngle = Quaternion.Euler (new Vector3 (0, angle, 0));
+                Quaternion relativeRotation = camRotationFlattened * quaternionAngle;
+                Quaternion lerpRotation = Quaternion.Lerp(transform.rotation, relativeRotation, Time.deltaTime * turnSpeed);
+
+                transform.rotation = lerpRotation;
+            }            
+            
+    }
+}
