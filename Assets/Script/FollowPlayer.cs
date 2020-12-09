d5c89c31d020849a6750e27ddb6bc88e083d41ef
aggge12 <axel.lundberg.gbg@hotmail.com>
2020-12-09 22:31:38 +0100

    added lotsa cool stuff

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    [SerializeField] Transform follow;
+
+    void LateUpdate()
+    {
+        transform.position = follow.position;
+        transform.rotation = follow.rotation;
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private NavMeshAgent agent;
     public int turnSpeed;
     public float speed;
     public Vector3 rotation;
-    public float velocity;
     private Vector3 Target;
     private float target;
     private float angle;
@@ -16,37 +17,52 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
-    void Update(){     
+    void Update(){ 
+        agent.updatePosition = true;
+        agent.updateRotation = true;
+
         float moveHorizontal = Input.GetAxisRaw ("Horizontal");
         float moveVertical = Input.GetAxisRaw ("Vertical");
 
+        if(Input.GetKey(KeyCode.Space)) CalcJump();
         if(moveHorizontal != 0 || moveVertical != 0){
             CalcRotation(moveHorizontal, moveVertical);
                     
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
          
         }
+        
 
     }
-        public void CalcRotation(float moveH, float moveV){
+    public void CalcRotation(float moveH, float moveV){
 
-            Vector3 camForward = Camera.main.transform.forward;
-            camForward.y = 0f;
-            Quaternion camRotationFlattened = Quaternion.LookRotation(camForward);
+        Vector3 camForward = Camera.main.transform.forward;
+        camForward.y = 0f;
+        Quaternion camRotationFlattened = Quaternion.LookRotation(camForward);
 
-            angle = Mathf.Atan2 (moveH, moveV) * Mathf.Rad2Deg; 
+        angle = Mathf.Atan2 (moveH, moveV) * Mathf.Rad2Deg; 
 
-            if (true){
-                Quaternion quaternionAngle = Quaternion.Euler (new Vector3 (0, angle, 0));
-                Quaternion relativeRotation = camRotationFlattened * quaternionAngle;
-                Quaternion lerpRotation = Quaternion.Lerp(transform.rotation, relativeRotation, Time.deltaTime * turnSpeed);
+        if (true){
+            Quaternion quaternionAngle = Quaternion.Euler (new Vector3 (0, angle, 0));
+            Quaternion relativeRotation = camRotationFlattened * quaternionAngle;
+            Quaternion lerpRotation = Quaternion.Lerp(transform.rotation, relativeRotation, Time.deltaTime * turnSpeed);
 
-                transform.rotation = lerpRotation;
-            }            
+            transform.rotation = lerpRotation;
+        }            
             
     }
+
+    public void CalcJump(){
+        agent.updatePosition = false;
+        agent.updateRotation = false;
+        agent.transform.position = new Vector3(agent.transform.position.x, transform.position.y, agent.transform.position.z);
+        Debug.Log("desired_velocity: " + agent.desiredVelocity.ToString());
+        Debug.Log("destination: " + agent.destination.ToString());
+        transform.position += new Vector3(0,1* Time.deltaTime,0);
+    }
+    
 }
